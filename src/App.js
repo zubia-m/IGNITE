d9bf49127ed6f948d41044c7fa77e6e3e2deb3e9
Zubia Muqtadir <zubia.muqtadr26@gmail.com>
2025-02-23 02:01:00 +0530

    Minor UI changes needed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./header"; // Import the Header component
+import HomeLanding from "./homeLanding"; // Import your HomeLanding component
+import SignInPage from "./signInPage"; // Import your SignInPage component
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router> {/* Wrap everything in Router */}
+      <Header /> {/* Header will have access to Router */}
+      <Routes>
+        <Route path="/" element={<HomeLanding />} /> {/* HomeLanding route */}
+        <Route path="/signin" element={<SignInPage />} /> {/* SignInPage route */}
+        <Route path="/realtors-contractors" element={<div>Realtors/Contractors Page</div>} />
+      </Routes>
+    </Router>
   );
 }
-
-export default App;
